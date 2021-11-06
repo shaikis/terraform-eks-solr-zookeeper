aa06328bd8dd10cef8a7bc6fa7d6cdb53dec8ffa
Ismails <shaik.urs@gmail.com>
2021-11-06 15:41:03 +0530

    added node group creation module

--- main.tf
+++ main.tf
@@ -6,7 +6,33 @@ module "eks_cluster" {
     eks_cluster_version  = "1.21"
 }
 
-module "eks-node-group" {
+module "zookeeper-node-group" {
+  source = "git@github.com:shaikis/terraform-eks-nodegroup.git"
+  cluster_name     = module.eks_cluster.eks_cluster_endpoint
+  subnet_ids       = ["subnet-b43efbfd", "subnet-22b4b17a"]
+
+  desired_size = 1
+  min_size     = 1
+  max_size     = 1
+
+  launch_template = {
+    id      = data.aws_launch_template.cluster.id
+    version = data.aws_launch_template.cluster.latest_version
+  }
+
+  labels = {
+    lifecycle = "OnDemand"
+  }
+
+  tags = {
+    "kubernetes.io/cluster/eks" = "owned"
+     Environment                 = "test"
+  }
+
+  depends_on = [data.aws_launch_template.cluster]
+}
+
+module "solr-node-group" {
   source = "git@github.com:shaikis/terraform-eks-nodegroup.git"
   cluster_name     = module.eks_cluster.eks_cluster_endpoint
   subnet_ids       = ["subnet-b43efbfd", "subnet-22b4b17a"]
